abd51ab7563aeaaaa9d4f6b84498adf8453d3e66
Maels Lafnegal <nice.noob1337@mail.ru>
2021-06-08 09:46:46 +0400

    added ViewModel for MainWindow

---
+++ MoldsApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using MoldsApp.Infrastructure.Commands;
+using MoldsApp.ViewModels.Base;
+
+namespace MoldsApp.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Экземпляры команд
+
+            CloseApplicationCommand = new LamdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
+    }
+}
