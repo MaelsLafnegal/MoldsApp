7d9f3f7fbf184ec5033a5523dd809ef791c86b42
Maels Lafnegal <nice.noob1337@mail.ru>
2021-06-08 11:09:15 +0400

    trying bind datagrid

--- MoldsApp/ViewModels/MainWindowViewModel.cs
+++ MoldsApp/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using MoldsApp.Data;
 using MoldsApp.Infrastructure.Commands;
+using MoldsApp.Models;
 using MoldsApp.ViewModels.Base;
 
 namespace MoldsApp.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Переменные
+
+        private List<Molds> _MoldsList;       
+        public List<Molds> MoldsList
+        {
+            get => _MoldsList;
+            set => Set(ref _MoldsList, ApplicationContext.GetContext().Molds.ToList());
+        }
+
         
+
+        #endregion
+
         #region Команды
 
         #region CloseApplicationCommand
